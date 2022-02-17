bc0f3d6f2f645d1f9c7140d54a6b81bf7c718fd9
bartoz1 <you@example.com>
2022-02-17 17:31:20 +0100

    added requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='raspberrydisplay',
+   version='0.5',
+   description='A simple module for displaying RaspberryPi 4 info onto OLED display',
+   author='RushKappa',
+   author_email='bartoszzylwis@gmail.com',
+   packages=find_packages(),
+   
+)
