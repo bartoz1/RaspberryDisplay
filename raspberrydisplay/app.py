ac106cfec4b29bc1bc140e5ca5d732e5f527a51d
bartoz1 <you@example.com>
2022-02-17 17:32:22 +0100

    added requirements

--- raspberrydisplay/app.py
+++ raspberrydisplay/app.py
@@ -1,10 +1,18 @@
 from raspberrydisplay.internet_connection import check_internet
 from raspberrydisplay.display import Display
+from gpiozero import Button
 import time
 
+button = Button(23)
+
 def run():
+    check_internet()
     display = Display()
     while True:
+        button.wait_for_press()
+        print("kliknieto przycisk!")
         check_internet()
+        
         display.refresh()
         time.sleep(30)
+        #time.sleep(30)
