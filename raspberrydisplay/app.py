695e010f5b4d5ac456590b86eedaf9bc889d7f4e
bartoz1 <nil112101@gmail.com>
2022-02-21 14:05:29 +0100

    added duration of displaying info

--- raspberrydisplay/app.py
+++ raspberrydisplay/app.py
@@ -2,18 +2,22 @@ import RPi.GPIO as GPIO
 from raspberrydisplay.display import Display
 from gpiozero import Button
 import time
+import configparser
 
-
-def use_display():
+def use_display(display_time):
     display = Display()
-    for i in range (5):
+    for i in range (display_time):
             display.refresh()
             time.sleep(1)
     display.turn_off()
     print("Display is off")
 
 def run():
-   
+    # getting app config
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    display_time = int(config['DISPLAY']['display_time'])
+
     GPIO.setmode(GPIO.BCM)  
   
     # GPIO 23 set up as input. It is pulled up to stop false signals  
@@ -23,7 +27,7 @@ def run():
         try:  
             time.sleep(1)
             GPIO.wait_for_edge(23, GPIO.FALLING)  
-            use_display()
+            use_display(display_time)
         except KeyboardInterrupt:  
             print("exiting...")
             GPIO.cleanup()       # clean up GPIO on CTRL+C exit 
