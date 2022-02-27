f7169355d6d7629d0966a72d7ef89c03895adfc3
bzylwis <nil112101@gmail.com>
2022-02-27 11:52:51 +0100

    updated requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
    name='raspberrydisplay',
@@ -6,6 +11,7 @@ setup(
    description='A simple module for displaying RaspberryPi 4 info onto OLED display',
    author='RushKappa',
    author_email='bartoszzylwis@gmail.com',
+   install_requires=required,
    packages=find_packages(),
    
 )
